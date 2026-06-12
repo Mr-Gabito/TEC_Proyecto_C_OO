fda9a3f4135573e1bc582b22467bf3ec9434b53d
Mr-Gabito <a01709333@tec.mx>
2026-06-12 12:00:09 -0600

    battlesim.h comments

--- battlesim.h
+++ battlesim.h
@@ -6,6 +6,7 @@
 #include <ctime>
 #include <vector>
 
+//uses a structure to get all the results necessary to send them to the main
 struct roundsim{
     int player_dmging;
     int enemy_dmging;
@@ -17,12 +18,13 @@ struct roundsim{
 
 class BattleSim{
     public:
-
+    //creates a vector including all the enemies, each a different class
     std::vector<Character*> enemies = {
         new Mage("Frieren", " the Slayer", 100, 5),
         new Assassin("Ezio", " Il Mentore", 150, 25),
         new Knight("Edward", " the Black Prince", 250, 15)
     };
+    //destructor that uses a for loop to delete the enemy based on how many there are inside the vector
     ~BattleSim(){
         for(Character* enemy:enemies){
             delete enemy;
@@ -32,7 +34,7 @@ class BattleSim{
 
 
 
-
+    //grabs the structure and uses a function to calculate the damage the enemy and player deals, whether either of them die, and the option selected
     roundsim figthing(Character* player, Character* enemy, int option){
         roundsim result = {0, 0, false, false, 0, 0};
         
@@ -43,6 +45,7 @@ class BattleSim{
             result.dead_enemy = true;
             result.enemy_dmging = 0;
             result.enemyoption = 0;
+            //returns all the results calculated for the player
             return result;
         }
 
@@ -52,6 +55,7 @@ class BattleSim{
         if(player -> get_health() <= 0){
             result.dead_player = true;
         }
+        //returns all the results calculated for the enemy
         return result;
     }
 };
